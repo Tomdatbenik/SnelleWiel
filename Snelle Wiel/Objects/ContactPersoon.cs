2b58fc659d2cd805f4487a471bdfb1c6ea63d16f
Tom <tomtrolt@gmail.com>
2017-12-25 22:12:12 +0100

    Klant start

---
+++ Snelle Wiel/Objects/ContactPersoon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snelle_Wiel.Objects
+{
+    class ContactPersoon
+    {
+
+        public string Naam { get; private set; }
+        public string Telefoon { get; private set; }
+        public string Email { get; private set; }
+
+        public ContactPersoon() { }
+
+        public ContactPersoon(string naam, string telefoon, string email)
+        {
+            this.Naam = naam;
+            this.Telefoon = telefoon;
+            this.Email = email;
+        }
+    }
+}
---
+++ Snelle Wiel/Objects/Locatie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snelle_Wiel.Objects
+{
+    public class Locatie
+    {
+        public string Plaats { get; private set; }
+        public string Adres { get; private set; }
+        public string Postcode { get; private set; }
+
+        public Locatie() { }
+
+        public Locatie(string loc,string adres, string postcode)
+        {
+            this.Plaats = loc;
+            this.Adres = adres;
+            this.Postcode = postcode;
+        }
+    }
+}
